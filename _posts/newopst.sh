1bac5be4bafd25aece4a50004f20c38592daa5b3
Zifan Ying <zifany4@illinois.edu>
2023-10-29 01:06:38 -0500

    add sh

---
+++ _posts/newopst.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo 'need filename and post title'
+    exit 1
+fi
+
+title=$2
+filename="$(dirname "$(realpath "$0")")"/$(date '+%Y-%m-%d-')${1// /-}.md
+
+echo $filename
+
+
+cat << EOF > $filename
+---
+titile: $title
+date: $(date '+%Y-%m-%d %H:%M:%S')
+math: true
+---
+
+EOF
+
